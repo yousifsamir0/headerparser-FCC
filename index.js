9e946dd39c42c9a882cece07aa2d233ce104a372
yousifsamir <yousifysm@gmail.com>
2024-11-29 03:15:05 +0200

    update index

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  
+  console.log(req.headers)
+  res.json({ 
+    ipaddress: req.ip,
+    language:req.get('accept-language'),
+    software:req.get('user-agent')
+   });
 });
 
 // listen for requests :)
